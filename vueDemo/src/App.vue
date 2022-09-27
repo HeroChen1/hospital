ff91f482542829897945cde736bfd8d3af8741b9
hero <978775772@qq.com>
2022-09-27 15:49:52 +0800

    first commit

---
+++ vueDemo/src/App.vue
@@ -0,0 +1,64 @@
+<!-- <template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style> -->
+<template>
+  <div id="app">
+    <div class="wrap">
+      <router-view v-if="isRouterAlive"></router-view>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'App',
+  provide () {
+    return {
+      reload: this.reload
+    }
+  },
+  data () {
+    return {
+      isRouterAlive: true
+    }
+  },
+  methods: {
+    reload () {
+      this.isRouterAlive = false
+      this.$nextTick(function() {
+         this.isRouterAlive = true
+      })
+    }
+  }
+}
+</script>
+<!-- <style>
+  #app{
+    position: relative;
+  }
+  @media only screen and (min-width: 1200px) {
+    .wrap{
+      width: 65%;
+      margin: 0 auto;
+    }
+  }
+</style> -->
